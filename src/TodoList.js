6c970ccd3aa5151c4c7ceafb09279b0576b46b16
Joey Mudd <jmudd@virtualpeaker.io>
2018-09-17 19:34:58 -0400

    Add a todo list component

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Todo1</li>
+        <li>Todo2</li>
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
